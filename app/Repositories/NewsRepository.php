09fb129868f008808e9ad622514df6068f289ede
ryohei-funamoto <r.funamoto242@gmail.com>
2025-06-26 07:36:36 +0900

    ニュースIDを取得する記述を追記

--- app/Repositories/NewsRepository.php
+++ app/Repositories/NewsRepository.php
@@ -7,7 +7,7 @@ use App\Models\News;
 class NewsRepository {
     public function getNewsList($limit)
     {
-        return News::select('news.created_at', 'news_categories.name as category_name', 'news.title')
+        return News::select('news.id', 'news.created_at', 'news_categories.name as category_name', 'news.title')
             ->leftJoin('news_categories', 'news.news_category_id', '=', 'news_categories.id')
             ->orderBy('news.created_at', 'desc')
             ->limit($limit)
