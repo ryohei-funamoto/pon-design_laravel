cf036d4dc9e3a32dc15e68cbe0683b56348b3897
Ryohei Funamoto <r.funamoto242@gmail.com>
2025-07-19 15:55:26 +0900

    contactsテーブルのダミーデータを作成

---
+++ database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Contact::factory()
+            ->count(100)
+            ->create();
+    }
+}
