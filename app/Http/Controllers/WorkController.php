35e75c18d82946aaa953d688136580df9ac09e4e
Ryohei Funamoto <r.funamoto242@gmail.com>
2025-07-20 12:23:33 +0900

    制作実績一覧ページに検索機能を追加

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -3,20 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\WorkRepository;
+use App\Repositories\WorksCategoryRepository;
 use Illuminate\Http\Request;
 
 class WorkController extends Controller
 {
     protected $work_repository;
+    protected $works_category_repository;
 
-    public function __construct(WorkRepository $work_repository)
-    {
+    public function __construct(
+        WorkRepository $work_repository,
+        WorksCategoryRepository $works_category_repository
+    ) {
         $this->work_repository = $work_repository;
+        $this->works_category_repository = $works_category_repository;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $work_list = $this->work_repository->getWorkList(limit: 9, use_pagination: true);
-        return view('works.index', ['work_list' => $work_list]);
+        $work_list = $this->work_repository->getWorkList(
+            limit: 9,
+            use_pagination: true,
+            keyword: $request->input('keyword'),
+            category: $request->input('category'),
+        );
+        $category_list = $this->works_category_repository->getAllCategories();
+        $param = [
+            'work_list' => $work_list,
+            'category_list' => $category_list,
+        ];
+        return view('works.index', $param);
     }
 }
